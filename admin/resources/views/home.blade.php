d7f5b2134b9312de038868789d76f7fdece35bcb
capitainn <morgantsloan@gmail.com>
2025-04-09 12:50:08 +0200

    creation de la barre de recherche non fonctionnel

--- admin/resources/views/home.blade.php
+++ admin/resources/views/home.blade.php
@@ -9,17 +9,66 @@
 <body class="bg-[url({{ asset('pictures/fond-connexion.avif') }})] bg-cover bg-no-repeat h-screen flex flex-col">
 
     <!-- Header -->
-    <header class="w-full bg-white shadow-md py-4 px-8 flex items-center">
+    <header class="w-full bg-white shadow-md py-4 px-8 flex items-center justify-between">
         <img src="{{ asset('pictures/logoLNT.jpg') }}" alt="Logo" class="h-10">        
-        <h1 class="ml-4 text-xl font-bold text-gray-800">Mon Application</h1>
+        <h1 class="ml-4 text-xl font-bold text-gray-800">Mon Portail Captif</h1>
+        <a href="" class="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+            <p>Mon compte</p>
+        </a>
     </header>
 
     <!-- Main Content -->
-    <main class="flex-grow flex items-center justify-center">
-        <div class="w-full max-w-md bg-white rounded-xl shadow-lg p-8">
-            <h2 class="text-2xl font-bold mb-6 text-center">Home</h2>
-            <p>{{ session('name') }}</p>
+    <main>
+    <section>
+        <div class="flex flex-row justify-center gap-2 m-5">
+            <!-- Barre de recherche -->
+            <div class="relative w-full max-w-md">
+
+                <button type="submit" 
+                    class="absolute left-2 top-1/2 transform -translate-y-1/2 bg-blue-600 text-white rounded-full p-2 hover:bg-blue-700 transition duration-200 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-4.35-4.35m0 0a7.5 7.5 0 1 0-10.6 0 7.5 7.5 0 0 0 10.6 0z" />
+                    </svg>
+                </button>
+
+                <input type="search" name="nom" id="nom" 
+                    class="w-full px-4 py-2 pl-12 pr-12 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400"
+                    placeholder="Recherchez un nom">
+
+                <button 
+                    class="absolute right-2 top-1/2 transform -translate-y-1/2 text-black p-2 hover:text-gray-600 transition duration-200 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+                    </svg>
+                </button>
+            </div>
         </div>
+        <article>
+            <table>
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2">Nom</th>
+                        <th class="px-4 py-2">Prénom</th>
+                        <th class="px-4 py-2">Email</th>
+                        <th class="px-4 py-2">Téléphone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td class="px-4 py-2">{{ $user->name }}</td>
+                            <td class="px-4 py-2">{{ $user->first_name }}</td>
+                            <td class="px-4 py-2">{{ $user->email }}</td>
+                            <td class="px-4 py-2">{{ $user->phone }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </article>
+    </section>
     </main>
 </body>
 </html>
