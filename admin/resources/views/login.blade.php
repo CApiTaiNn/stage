989df707a04f7b5e1687431b15fdd3d2e3b8102b
capitainn <morgantsloan@gmail.com>
2025-04-11 15:53:42 +0200

    fond sur la page de connexion ajouter

--- admin/resources/views/login.blade.php
+++ admin/resources/views/login.blade.php
@@ -6,16 +6,16 @@
     <title>Connexion</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-[url({{ asset('pictures/fond-connexion.avif') }})] bg-cover bg-no-repeat h-screen flex flex-col">
+<body class="h-screen flex flex-col">
 
     <!-- Header -->
-    <header class="w-full bg-white shadow-md py-4 px-8 flex items-center">
+    <header class="w-full bg-white shadow-md py-4 px-8 flex items-center justify-between">
         <img src="{{ asset('pictures/logoLNT.jpg') }}" alt="Logo" class="h-10">        
-        <h1 class="ml-4 text-xl font-bold text-gray-800">Mon Application</h1>
+        <h1 class="ml-4 text-xl font-bold text-gray-800">Mon Portail Captif</h1>
     </header>
 
     <!-- Main Content -->
-    <main class="flex-grow flex items-center justify-center">
+    <main class="flex-grow flex items-center justify-center bg-login bg-cover bg-center">        
         <div class="w-full max-w-md bg-white rounded-xl shadow-lg p-8">
             <h2 class="text-2xl font-bold mb-6 text-center">Connexion à votre compte gérant</h2>
             <form method="POST" action="{{ route('loginApi') }}">
